1a1f4ab43074e84037d8b8cf963c9a8f8092247c
ThaVip <sheriffmusazarami@gmail.com>
2024-08-16 15:42:15 -0700

    find using python3

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,18 +1,12 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+
+
 def list_all(mongo_collection):
-    """
-    Lists all documents in a collection.
+    """ List all documents in Python """
+    documents = mongo_collection.find()
 
-    Args:
-        mongo_collection: The pymongo collection object
-
-    Returns:
-        A list of all documents in the collection. If no documents are found, returns an empty list.
-    """
-    # Count documents
-    document_count = mongo_collection.count_documents({})
-    if document_count == 0:
+    if documents.count() == 0:
         return []
-    
-    # Retrieve all documents and convert the cursor to a list
-    return list(mongo_collection.find())
 
+    return documents
